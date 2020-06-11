e4a6fd5f377e5682ccd630d26136094299102e9d
HINA KHADIM <hinakhadim2002@gmail.com>
2020-06-11 09:20:44 +0500

    applying light and dark mode

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Room from './components/room';
 function App() {
 
   const [light, setlight] = useState(false);
+  const [mode, setMode] = useState(false)
 
   function changeState() {
     console.log("button clicked")
@@ -13,13 +14,17 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className={`App ${
+      mode ? "light" : "dark"
+      }`}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is React App deployed on Surge
         </p>
         <button onClick={changeState}>Light On</button>
+        <br></br>
+        <button onClick={() => setMode(!mode)}>Change the mode</button>
         <p>The light is : {light ? "on" : "off"}</p>
       </header>
       <Room />
