3b885655726b75b44b01c54c1f393776e454edb3
HINA KHADIM <hinakhadim2002@gmail.com>
2020-07-08 17:50:20 +0500

    add animation through js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [items, setItems] = useState(true);
+  // const [items, setItems] = useState(true);
 
-  useEffect(() => {
-    console.log("Hina is useeffect");
-  }, []);
+  // window.addEventListener("load", function () {
+  //   var shapeElement = document.querySelector(".shape");
+  //   var frames = [
+  //     { background: "red", transform: "translateX(0px)" },
+  //     { background: "Yellow" },
+  //     { background: "blue", transform: "translateX(500px)" },
+  //   ];
 
-  return <div onClick={() => setItems(!items)}>Home</div>;
+  //   var timing = {
+  //     duration: 1000,
+  //     iterations: Infinity,
+  //     direction: "alternate",
+  //   };
+
+  //   shapeElement.animate(frames, timing);
+  //   // animation.play(); animation.pause(); animation.reverse(); animation.updatePlaybackRate(animation.playbackRate * 1.2)
+  // });
+
+  // useEffect(() => {
+  //   console.log("Hina is useeffect");
+  // }, []);
+
+  return (
+    <div className="red shape">
+      Wellcome World
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </div>
+  );
 }
 
 export default App;
