fdb9b009fd4603261eafcc4a5a5e3a8204d8e6a9
HINA KHADIM <hinakhadim2002@gmail.com>
2020-07-13 22:23:09 +0500

    animation in react

---
+++ src/Animation.js
@@ -0,0 +1,29 @@
+function App() {
+  // const [items, setItems] = useState(true);
+
+  // window.addEventListener("load", function () {
+  //   var shapeElement = document.querySelector(".shape");
+  //   var frames = [
+  //     { background: "red", transform: "translateX(0px)" },
+  //     { background: "Yellow" },
+  //     { background: "blue", transform: "translateX(500px)" },
+  //   ];
+
+  //   var timing = {
+  //     duration: 1000,
+  //     iterations: Infinity,
+  //     direction: "alternate",
+  //   };
+
+  //   shapeElement.animate(frames, timing);
+  //   // animation.play(); animation.pause(); animation.reverse(); animation.updatePlaybackRate(animation.playbackRate * 1.2)
+  // });
+
+  // useEffect(() => {
+  //   console.log("Hina is useeffect");
+  // }, []);
+
+  return <div className="red shape"></div>;
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./App.css";
 
+import useWebAnimations, { bounce } from "@wellyshen/use-web-animations";
+
 function App() {
-  // const [items, setItems] = useState(true);
+  const { ref } = useWebAnimations({ ...bounce });
 
-  // window.addEventListener("load", function () {
-  //   var shapeElement = document.querySelector(".shape");
-  //   var frames = [
-  //     { background: "red", transform: "translateX(0px)" },
-  //     { background: "Yellow" },
-  //     { background: "blue", transform: "translateX(500px)" },
-  //   ];
-
-  //   var timing = {
-  //     duration: 1000,
-  //     iterations: Infinity,
-  //     direction: "alternate",
-  //   };
-
-  //   shapeElement.animate(frames, timing);
-  //   // animation.play(); animation.pause(); animation.reverse(); animation.updatePlaybackRate(animation.playbackRate * 1.2)
+  // const { ref, playState, getAnimation } = useWebAnimations({
+  //   keyframes: [
+  //     { transform: "translate(0px)", background: "red" }, // Move by 500px
+  //     { transform: "translate(500px)", background: "green" }, // Go through three colors
+  //   ],
+  //   timing: {
+  //     delay: 500, // Start with a 500ms delay
+  //     duration: 1000, // Run for 1000ms
+  //     iterations: 2, // Repeat once
+  //     direction: "alternate", // Run the animation forwards and then backwards
+  //     easing: "ease-in-out", // Use a fancy timing function
+  //   },
+  //   //onReady: ({ playState, animate, animation }) => {
+  //   // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+  //   //},
+  //   onUpdate: ({ playState, animate, animation }) => {
+  //     console.log("Play State", playState);
+  //     console.log("animate", animate);
+  //     console.log("animation", animation);
+  //     // Triggered when the animation enters the running state or changes state
+  //   },
+  //   //onFinish: ({ playState, animate, animation }) => {
+  //   // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+  //   //},
+  //   // More useful options...
   // });
 
-  // useEffect(() => {
-  //   console.log("Hina is useeffect");
-  // }, []);
+  // const input = useRef(null);
 
+  // useEffect(() => {
+  //   input.current.focus();
+  // }, []);
   return (
-    <div className="red shape">
-      Wellcome World
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+    <div>
+      <p>Hello World, </p>
+      <div ref={ref}>Hello again!</div>
+      {/* <input ref={ref} type="text" /> */}
+
+      {/* <button
+        onClick={() => {
+          getAnimation().play();
+        }}
+      >
+        Play
+      </button>
+      <button
+        onClick={() => {
+          getAnimation().pause();
+        }}
+      >
+        Pause
+      </button> */}
     </div>
   );
 }
